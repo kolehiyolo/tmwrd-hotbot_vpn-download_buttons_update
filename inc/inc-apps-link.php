048822c447dfc4d2ce22a0fb02b4440af8ebb4bc
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-01-18 17:45:40 +0800

    Done

--- inc/inc-apps-link.php
+++ inc/inc-apps-link.php
@@ -4,12 +4,14 @@
     <p><?php esc_html_e('Download our apps for iOS and Android', 'hotbot' ); ?></p>
     <div class="btn-group btn-group--center">
       <a id="playstore" href="<?php  echo $playUrl; ?>">
-        <img loading=lazy src="<?php bloginfo('template_url'); ?>/assets/img/google-play.svg" alt="google-play" />
+        <!-- <img loading=lazy src="<?php bloginfo('template_url'); ?>/assets/img/google-play.svg" alt="google-play" /> -->
+        <img loading=lazy src="<?php bloginfo('template_url'); ?>/assets/img/download-buttons/google-play/google_play-<?php echo ICL_LANGUAGE_CODE ?>.png" alt="google-play" />
       </a>
       <a id="appstore" href="https://apps.apple.com/us/app/hotbot-vpn/id1488136863" target="_blank">
-        <img loading=lazy src="<?php bloginfo('template_url'); ?>/assets/img/app-store.svg" alt="app-store" />
+        <!-- <img loading=lazy src="<?php bloginfo('template_url'); ?>/assets/img/app-store.svg" alt="app-store" /> -->
+        <img loading=lazy src="<?php bloginfo('template_url'); ?>/assets/img/download-buttons/apple-app-store/app_store_badge-<?php echo ICL_LANGUAGE_CODE ?>.svg" alt="google-play" />
       </a>
     </div>
   </div>
 </section>
-<?php include 'inc-utm.php'; ?>
+<?php include 'inc-utm.php'; ?> 
